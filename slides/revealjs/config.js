7d721cac9a4795b7df876e32c1a4123f7633e1e0
Nils Hartmann <nils@nilshartmann.net>
2022-04-15 10:52:39 +0200

    slides to own folder, config

--- slides/revealjs/config.js
+++ slides/revealjs/config.js
@@ -6,44 +6,53 @@ const isLocalServer =
   window.location.hostname.indexOf("localhost") !== -1 ||
   window.location.hostname.indexOf("127.0.0.1") !== -1;
 
-Reveal.addEventListener("ready", function (event) {
-  console.log("CONFIGURE.........................", isLocalServer)
+// Reveal.addEventListener("ready", function (event) {
 
-  if (isLocalServer) {
-    // only applies to presentation version
-    Reveal.configure({ controls: false });
-  }
-
-  // open all externals link in new tab
-  document.querySelectorAll('a:not([href^="#"])').forEach(
-    i => {
-      i.setAttribute("target", "_blank")
-    }
-  )
-
-  document.querySelectorAll('pre code').forEach(
-    n => {
-      n.setAttribute("contenteditable", "true");
-//      n.setAttribute("data-trim", "")
-    }
-  )
-});
-
-
-
-//$('a:not([href^="#"])').attr("target", "_blank");
-//$("pre code").attr("contenteditable", "true");
-//$("pre code").attr("data-trim", "");
+// });
 
 Reveal.initialize({
   hash: true,
+  fragmentInURL: false,
+  fragments: false,
 
-//  highlight: {
-//    beforeHighlight: hljs => hljs.registerLanguage(/*...*/)
-//  },
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+
+  width: "100%",
+  height: "100%",
+  hideInactiveCursor: false,
 
   // Learn about plugins: https://revealjs.com/plugins/
-  plugins: [RevealMarkdown, RevealHighlight, RevealNotes]
+  plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
+}).then(() => {
+  if (isLocalServer) {
+    // only applies to presentation version
+    Reveal.configure({ controls: false, fragments: true });
+
+    document.querySelectorAll("li").forEach((n) => n.classList.add("fragment"));
+
+    document.querySelectorAll("pre.fragment").forEach((n) => {
+      n.classList.remove("fragment");
+      const div = document.createElement("div");
+      div.classList.add("fragment");
+      n.parentNode.insertBefore(div, n);
+      div.appendChild(n);
+    });
+  }
+
+  // document.querySelectorAll("code[data-line-numbers]").forEach((el) => {
+  //   delete el.dataset.lineNumbers;
+  //   console.log(el.dataset);
+  // });
+
+  // open all externals link in new tab
+  document.querySelectorAll('a:not([href^="#"])').forEach((i) => {
+    i.setAttribute("target", "_blank");
+  });
+
+  document.querySelectorAll("pre code").forEach((n) => {
+    n.setAttribute("contenteditable", "true");
+  });
 });
-
-
