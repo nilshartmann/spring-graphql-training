5d6d2ec8675ae454194bc12482f28883d191393f
Nils Hartmann <nils@nilshartmann.net>
2022-04-19 09:46:09 +0200

    fixes

--- slides/revealjs/config.js
+++ slides/revealjs/config.js
@@ -6,13 +6,12 @@ const isLocalServer =
   window.location.hostname.indexOf("localhost") !== -1 ||
   window.location.hostname.indexOf("127.0.0.1") !== -1;
 
-// Reveal.addEventListener("ready", function (event) {
-
-// });
-
 Reveal.initialize({
   hash: true,
+  // do not change url for each fragment that is visible
   fragmentInURL: false,
+
+  // do not use fragments at all in 'public' version
   fragments: false,
 
   controls: true,
@@ -20,8 +19,12 @@ Reveal.initialize({
   history: true,
   center: true,
 
+  // i need all space i can get
   width: "100%",
   height: "100%",
+
+  // there's a bug with Firefox that does not show
+  // the cursor again after is has been hidden, so disable
   hideInactiveCursor: false,
 
   // Learn about plugins: https://revealjs.com/plugins/
@@ -29,21 +32,23 @@ Reveal.initialize({
 }).then(() => {
   if (isLocalServer) {
     // only applies to presentation version
+
+    // As a presenter I don't need controls but fragments
     Reveal.configure({ controls: false, fragments: true });
 
+    // every 'li' should become a fragment
     document.querySelectorAll("li").forEach((n) => n.classList.add("fragment"));
-    // document.querySelectorAll("li.fragment:first-child").forEach((n) => {
-    //   n.classList.remove("fragment");
-    //   n.parentNode.classList.add("fragment");
-    // });
 
     // when using code blocks with markdown like this:
     //  ```java fragment
-    //  the code elements gets the "fragment" class
+    //  the <code> elements gets the "fragment" class, but it needs
+    //  to be on its parent <pre> element
     document.querySelectorAll("code.fragment").forEach((n) => {
       n.classList.remove("fragment");
       n.parentNode.classList.add("fragment");
     });
+    // surround all pre.fragment blocks with a 'div.fragment'
+    // (somehow this seems to work better with code fragments)
     document.querySelectorAll("pre.fragment").forEach((n) => {
       n.classList.remove("fragment");
       const div = document.createElement("div");
@@ -52,25 +57,27 @@ Reveal.initialize({
       div.appendChild(n);
     });
 
+    // li having a code inside, should not have a bullet point (see also styles.css)
     document.querySelectorAll("li > pre.code-wrapper").forEach((n) => {
       n.parentNode.classList.add("no-icon");
     });
   } else {
     // public version
+
+    // remove presenter "demo" marks (children of demo)
     document.querySelectorAll(".demo .demo").forEach((n) => n.remove());
+    // remove todos
     document.querySelectorAll(".todo").forEach((n) => n.remove());
   }
 
-  // document.querySelectorAll("code[data-line-numbers]").forEach((el) => {
-  //   delete el.dataset.lineNumbers;
-  //   console.log(el.dataset);
-  // });
+  // Changes for both public and presenter version
 
   // open all externals link in new tab
   document.querySelectorAll('a:not([href^="#"])').forEach((i) => {
     i.setAttribute("target", "_blank");
   });
 
+  // make all code editable
   document.querySelectorAll("pre code").forEach((n) => {
     n.setAttribute("contenteditable", "true");
   });
