8f17c468c8d9dd80fcd0c33ff4cb9b43988a4024
Nils Hartmann <nils@nilshartmann.net>
2022-07-17 08:37:08 +0200

    Add complexity directive example

--- app/publy-backend/src/main/java/nh/publy/backend/graphql/runtime/PublyGraphQLConfig.java
+++ app/publy-backend/src/main/java/nh/publy/backend/graphql/runtime/PublyGraphQLConfig.java
@@ -1,6 +1,11 @@
 package nh.publy.backend.graphql.runtime;
 
+import graphql.analysis.FieldComplexityCalculator;
+import graphql.analysis.FieldComplexityEnvironment;
+import graphql.analysis.MaxQueryComplexityInstrumentation;
 import graphql.analysis.MaxQueryDepthInstrumentation;
+import graphql.language.IntValue;
+import graphql.schema.GraphQLDirective;
 import graphql.schema.GraphQLScalarType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +37,43 @@ public class PublyGraphQLConfig {
   @Bean
   public MaxQueryDepthInstrumentation maxQueryDepthInstrumentation() {
     return new MaxQueryDepthInstrumentation(20);
+  }
+
+  // Enable only during workshops and demos!
+//  @Bean
+//  public MaxQueryComplexityInstrumentation maxQueryComplexityInstrumentation() {
+//    return new MaxQueryComplexityInstrumentation(60, new PublyFieldComplexityCalculator());
+//  }
+
+  /**
+   * Enhancement of a FieldComplexityCalculator that allows you to specify the
+   * "complexity" of a field using a custom "Complexity" directive in your schema.
+   *
+   * This way you can make fields that require more backend resources when requested
+   * more "complex", for example fields that needs additional database or
+   * service calls.
+   *
+   * <code>
+   *   type Pet {
+   *     name: String! # Default Complexity: 1
+   *     owner: PetOwner! @Complexity(complexity: 10)
+   *   }
+   * </code>
+   */
+  class PublyFieldComplexityCalculator implements FieldComplexityCalculator {
+    @Override
+    public int calculate(FieldComplexityEnvironment environment, int childComplexity) {
+      GraphQLDirective complexity = environment.getFieldDefinition().getDirective("Complexity");
+      if (complexity != null) {
+        IntValue value = complexity
+          .getArgument("complexity")
+          .toAppliedArgument()
+          .getValue();
+
+        return value.getValue().intValue() + childComplexity;
+      }
+      return 1 + childComplexity;
+    }
   }
 
 //  @Bean
