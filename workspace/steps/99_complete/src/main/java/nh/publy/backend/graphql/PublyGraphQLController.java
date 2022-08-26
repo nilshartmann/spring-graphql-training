02d8a35d4625e4b0d08f7ea1a4881af979ca6d1d
Nils Hartmann <nils@nilshartmann.net>
2022-08-26 21:02:17 +0200

    complete aktualisiert

--- workspace/steps/99_complete/src/main/java/nh/publy/backend/graphql/PublyGraphQLController.java
+++ workspace/steps/99_complete/src/main/java/nh/publy/backend/graphql/PublyGraphQLController.java
@@ -3,84 +3,61 @@ package nh.publy.backend.graphql;
 import nh.publy.backend.domain.*;
 import nh.publy.backend.domain.user.User;
 import nh.publy.backend.domain.user.UserService;
-import nh.publy.backend.util.DemoConfig;
+import org.dataloader.DataLoader;
+import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.*;
 import org.springframework.graphql.execution.BatchLoaderRegistry;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import reactor.core.publisher.Flux;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 @Controller
 public class PublyGraphQLController {
-
   private static final Logger log = LoggerFactory.getLogger(PublyGraphQLController.class);
-
   private final StoryRepository storyRepository;
   private final UserService userService;
   private final PublyDomainService publyDomainService;
 
-  public PublyGraphQLController(StoryRepository storyRepository, UserService userService, BatchLoaderRegistry registry, PublyDomainService publyDomainService) {
+  public PublyGraphQLController(StoryRepository storyRepository, UserService userService, PublyDomainService publyDomainService, BatchLoaderRegistry batchLoaderRegistry) {
     this.storyRepository = storyRepository;
     this.userService = userService;
-//    registry.forTypePair(String.class, User.class).registerBatchLoader(
-//      (List<String> keys, BatchLoaderEnvironment env) -> {
-//        log.info("Loading Users with keys {}", keys);
-//        return userService.findUsers(keys);
-//      }
-//    );
     this.publyDomainService = publyDomainService;
+
+    batchLoaderRegistry.forTypePair(String.class, User.class).registerBatchLoader((keys, env) -> {
+      log.info("Loading Users with keys {}", keys);
+      return userService.findUsers(keys);
+    });
   }
 
   @QueryMapping
   public List<Story> stories() {
-    return this.storyRepository.findAll();
+    return storyRepository.findAll();
   }
 
   @QueryMapping
   public Optional<Story> story(@Argument Long storyId) {
-    return this.storyRepository.findById(storyId);
+    return storyRepository.findById(storyId);
   }
 
   @SchemaMapping
-  CompletableFuture<String> excerpt(Story story, @Argument int maxLength) {
-    return CompletableFuture.supplyAsync(() -> {
-      DemoConfig.slowDown("Reading Excerpt for story " + story.getId());
-      return story.getBody().substring(0, maxLength);
-
-    });
+  public CompletableFuture<User> user(Member member, DataLoader<String, User> userDataLoader) {
+    String userId = member.getUserId();
+    log.info("Delegating load with user id {} to DataLoader", userId);
+    return userDataLoader.load(userId);
   }
-
-  @BatchMapping
-  Flux<User> user(List<Member> member) {
-    List<String> keys = member.stream().map(Member::getUserId).collect(Collectors.toList());
-    log.info("Loading Users with BatchMapping for keys {}", keys);
-    return userService.findUsers(keys);
-  }
-
-//  @SchemaMapping
-//  CompletableFuture<User> user(Member member, DataLoader<String, User> dataLoader) {
-//    return dataLoader.load(member.getUserId());
-//  }
 
   static class AddCommentInput {
     private final Long storyId;
-    private final Long memberId;
-    private final @Size(min=5) String content;
+    private final String content;
 
-    public AddCommentInput(Long storyId, Long memberId, String content) {
+    public AddCommentInput(Long storyId, String content) {
       this.storyId = storyId;
-      this.memberId = memberId;
       this.content = content;
     }
 
@@ -88,27 +65,62 @@ public class PublyGraphQLController {
       return storyId;
     }
 
-    public Long getMemberId() {
-      return memberId;
-    }
-
     public String getContent() {
       return content;
     }
   }
 
-  @Autowired MemberRepository memberRepository;
-
-  @MutationMapping
-  @PreAuthorize("hasRole('ROLE_USER')")
-  public Comment addComment(@Valid @Argument AddCommentInput input,
-                            @AuthenticationPrincipal User user) {
-    Long memberIdForUserId = memberRepository.getMemberIdForUserId(user.getId());
-    return publyDomainService.addComment(
-      input.getStoryId(),
-      memberIdForUserId,
-      input.getContent()
-    );
+  interface AddCommentPayload {
   }
 
+  static class AddCommentSuccessPayload implements AddCommentPayload{
+    private final Comment newComment;
+
+    public AddCommentSuccessPayload(Comment newComment) {
+      this.newComment = newComment;
+    }
+
+    public Comment getNewComment() {
+      return newComment;
+    }
+  }
+
+  static class AddCommentFailedPayload implements AddCommentPayload {
+    private final String errorMsg;
+
+
+    AddCommentFailedPayload(String errorMsg) {
+      this.errorMsg = errorMsg;
+    }
+
+    public String getErrorMsg() {
+      return errorMsg;
+    }
+  }
+
+  @MutationMapping
+  @PreAuthorize("hasRole('USER')")
+  public AddCommentPayload addComment(@Argument AddCommentInput input, @AuthenticationPrincipal User user) {
+
+    Long memberId = publyDomainService.getMemberForUser(user);
+
+    try {
+      Comment newComment = publyDomainService.addComment(
+        input.getStoryId(),
+        memberId,
+        input.getContent()
+      );
+
+      return new AddCommentSuccessPayload(newComment);
+    } catch (Exception ex) {
+      log.error("Add Comment failed: {}", ex.getMessage(), ex);
+      return new AddCommentFailedPayload(ex.getMessage());
+    }
+  }
+
+  @SubscriptionMapping
+  public Publisher<OnNewCommentEvent> onNewComment(@Argument Long storyId) {
+    return this.publyDomainService.getOnNewCommentEventPublisher()
+      .filter(e -> e.getStoryId().equals(storyId));
+  }
 }
