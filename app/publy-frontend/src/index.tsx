c4a6d32455c3be21c62d8102c86ee9ecfe0ab971
Nils Hartmann <nils@nilshartmann.net>
2022-08-26 21:10:17 +0200

    Prettier 🥳

--- app/publy-frontend/src/index.tsx
+++ app/publy-frontend/src/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
-import {BrowserRouter} from "react-router-dom";
-import {ApolloProvider,} from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
 import App from "./App";
 import "./index.css";
-import {createApolloClient} from "./create-apollo-client";
+import { createApolloClient } from "./create-apollo-client";
 
 const apolloClient = createApolloClient();
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-	<BrowserRouter>
-		<ApolloProvider client={apolloClient}>
-			<App/>
-		</ApolloProvider>
-	</BrowserRouter>
+  <BrowserRouter>
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
 );
