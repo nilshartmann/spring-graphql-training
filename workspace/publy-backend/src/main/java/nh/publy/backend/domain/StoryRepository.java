3546fd1f6177b5a80b93a99f2e248883ee32cbbd
Nils Hartmann <nils@nilshartmann.net>
2023-10-17 17:23:02 +0200

    Spring Boot 3.2M-3 with Limit Support in Repos

--- workspace/publy-backend/src/main/java/nh/publy/backend/domain/StoryRepository.java
+++ workspace/publy-backend/src/main/java/nh/publy/backend/domain/StoryRepository.java
@@ -1,10 +1,16 @@
 package nh.publy.backend.domain;
 
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.ScrollPosition;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Window;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface StoryRepository extends JpaRepository<Story, Long> {
   @Query(value = "SELECT nextval('story_ids');", nativeQuery = true)
   Long getNextStoryId();
+
+  Window<Story> findAllBy(ScrollPosition scrollPosition, Limit limit, Sort sort);
 }
 
